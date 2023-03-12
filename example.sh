53dfa8ef10bcefa795f1ab27088c1dd606cc5d51
d-dum <levdenisov666@gmail.com>
2023-03-12 14:06:38 +0100

    header parsing

--- example.sh
+++ example.sh
@@ -4,10 +4,17 @@
 source "./lib.sh"
 
 # $1 json request body
+# $2 header JSON object
 function helloRequestHandler {
-    resp=$(http_response "<p>${1}<p>" 'text/html' '200')
+    resp=$(http_response "<p>${2}<p><p>${1}<p>" 'text/html' '200')
+    echo "${resp}"
+}
+
+function helloGetRequestHandler {
+    resp=$(http_response "${2}" "text/html" "200")
     echo "${resp}"
 }
 
 add_handler "POST" "/" "$(declare -f helloRequestHandler)" helloRequestHandler
+add_handler "GET" "/" "$(declare -f helloGetRequestHandler)" helloGetRequestHandler
 start_server 8080
--- lib.sh
+++ lib.sh
@@ -11,10 +11,31 @@ declare -A HTTP_HANDLERS=()
 # $3 = handler body (function body) see example
 # $4 = handler name
 function add_handler {
-    METHOD="${1}"
-    HANDLER_PATH="${2}"
-    REQUEST_PATH="${METHOD} ${HANDLER_PATH}"
+    local METHOD="${1}"
+    local HANDLER_PATH="${2}"
+    local REQUEST_PATH="${METHOD} ${HANDLER_PATH}"
     HTTP_HANDLERS["${REQUEST_PATH}"]="${3};${4}"
+}
+
+function parse_headers {
+    declare -a argAry1=("${!1}")
+    local out_headers="{"
+    local first=true
+    IFS=": "
+    for header in "${!argAry1[@]}"; do
+        read -ra header_arr <<< "${argAry1[${header}]}"
+        local key="${header_arr[0]}"
+        local val="${header_arr[1]}"
+        if [[ "${first}" = true ]]
+        then
+            out_headers="${out_headers}\"${key}\": \"${val}\""
+            first=false
+        else
+            out_headers="${out_headers},\"${key}\": \"${val}\""
+        fi
+    done
+    out_headers="${out_headers}}"
+    echo "${out_headers}"
 }
 
 # $1 = response body
@@ -22,9 +43,9 @@ function add_handler {
 # $3 = status
 function http_response {
     # TODO: headers
-    HTTP_HEAD="HTTP/1.1"
-    body="${1}"
-    status="${3}"
+    local HTTP_HEAD="HTTP/1.1"
+    local body="${1}"
+    local status="${3}"
     if [[ ${status} = "" ]]
     then
         status="200"
@@ -37,7 +58,7 @@ function http_response {
             *) status="200 OK" 
     esac
 
-    content_type="${2}"
+    local content_type="${2}"
 
     if [[ ${status} = "200 OK" ]]
     then
@@ -48,17 +69,24 @@ function http_response {
 }
 
 function general_request_handler {
+    local headers=()
+    local REQUEST=""
+    local CONTENT_LENGTH=""
+    local JSON=""
     while read -r line; do
         echo "${line}"
         trline=$(echo "${line}" | tr -d '\r\n')
 
         [[ -z "${trline}" ]] && break
 
-        HEADLINE_REGEX='(.*?)\s(.*?)\sHTTP.*?'
+        [[ ! "${trline}" =~ ${HEADLINE_REGEX} ]] &&
+            headers+=("${trline}")
+        
+        local HEADLINE_REGEX='(.*?)\s(.*?)\sHTTP.*?'
         [[ "${trline}" =~ ${HEADLINE_REGEX} ]] &&
             REQUEST=$(echo "${trline}" | sed -E "s/${HEADLINE_REGEX}/\1 \2/")
         
-        CONTENT_LENGTH_REGEX='Content-Length:\s(.*?)'
+        local CONTENT_LENGTH_REGEX='Content-Length:\s(.*?)'
         [[ "${trline}" =~ ${CONTENT_LENGTH_REGEX} ]] &&
             CONTENT_LENGTH=$(echo "${trline}" | sed -E "s/${CONTENT_LENGTH_REGEX}/\1/")
     done
@@ -74,16 +102,20 @@ function general_request_handler {
     fi
 
     echo "${JSON}"
+    local parsed_headers=""
+    parsed_headers=$(parse_headers headers[@])
+    
 
     # case "${REQUEST}" in
     #     "GET /") RESPONSE="HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n</h1>PONG</h1>" ;;
     #           *) RESPONSE="HTTP/1.1 404 NotFound\r\n\r\n\r\nNot Found" ;;
     # esac
+    local RESPONSE=""
 
     if [[ -n "${HTTP_HANDLERS[${REQUEST}]}" ]]
     then
-        handler="${HTTP_HANDLERS[${REQUEST}]}"
-        RESPONSE=$(eval "${handler} '${JSON}'")
+        local handler="${HTTP_HANDLERS[${REQUEST}]}"
+        RESPONSE=$(eval "${handler} '${JSON}' '${parsed_headers}'")
     else
         RESPONSE="HTTP/1.1 404 NotFound\r\n\r\n\r\nNot Found"
     fi
